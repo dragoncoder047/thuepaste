ffe42a12fd9bc8a1340370c14648e6242576f644
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-11 09:47:43 -0500

    Create doesithalt.js

---
+++ doesithalt.js
@@ -0,0 +1,14 @@
+function halts(thue, timeout = 60000) {
+    const start = +new Date();
+    function chanceOfHalting(text) {
+        if (+new Date() - start > timeout) throw 'timeout';
+        var matches = thue.matches(text);
+        if (matches.length === 0) return 1.0;
+        var chances = [];
+        for (var [r, m] of matches) {
+            chances.push(chanceOfHalting(thue.apply(r, m, text)));
+        }
+        return chances.reduce((a, b) => a + b, 0.0) / chances.length;
+    }
+    return chanceOfHalting(thue.text);
+}
