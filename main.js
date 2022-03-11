2e37e660bcaadabf3d8a17bf72857debbc858801
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-11 08:17:46 -0500

    Back to textarea

--- main.js
+++ main.js
@@ -33,9 +33,9 @@ async function init(filename=false) {
             return;
         }
         text = await resp.text();
-        codeBox.textContent = text;
+        codeBox.value = text;
     } else
-        text = codeBox.textContent;
+        text = codeBox.value;
     status('Parsing...');
     var [rules, init] = parse(text, [OutputRule, InputRule, Rule]);
     thue.init(init);
