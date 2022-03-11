7c69238835dfdf44c14a52283b3eb1afe213c3a6
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-11 11:08:13 -0500

    revise algorithm

--- doesithalt.js
+++ doesithalt.js
@@ -1,23 +1,24 @@
 function chanceOfHalting(thue, timeout = 5000) {
     const start = +new Date();
-    function hc(text) {
+    function hc(text, seenRules) {
         if (+new Date() - start > timeout) throw 'timeout';
         var matches = thue.matches(text);
         if (matches.length === 0) return 1.0; // will defintely halt.
         var chances = [];
         for (var [r, m] of matches) {
             var applied = thue.apply(r, m, text, true);
-            // test to see if the rule will match itself
+            // test to see if the rule will match itself or in a cycle
             var oldRules = thue.rules;
-            thue.rules = [r];
-            var mematch = thue.matches(applied).length;
+            var moreSeen = seenRules.indexOf(r) === -1 ? [r].concat(seenRules) : seenRules;
+            thue.rules = moreSeen;
+            var cmatch = thue.matches(applied).length;
             thue.rules = oldRules;
-            if (mematch > 0)
-                chances.push(1.0 - (mematch / matches.length)); // may match itself this many times, so it might not halt.
+            if (cmatch > 0)
+                chances.push(1.0 - (cmatch / matches.length)); // may match itself this many times, so it might not halt.
             else
-                chances.push(hc(thue.apply(r, m, text, true)));
+                chances.push(hc(thue.apply(r, m, text, true), moreSeen));
         }
         return chances.reduce((a, b) => a + b, 0.0) / chances.length;
     }
-    return hc(thue.text);
+    return hc(thue.text, []);
 }
