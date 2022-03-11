94f7f9403d5716025192aab1dc7f9675cd7ec232
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-11 10:46:28 -0500

    renaming

--- doesithalt.js
+++ doesithalt.js
@@ -1,6 +1,6 @@
-function halts(thue, timeout = 5000) {
+function chanceOfHalting(thue, timeout = 5000) {
     const start = +new Date();
-    function chanceOfHalting(text) {
+    function hc(text) {
         if (+new Date() - start > timeout) throw 'timeout';
         var matches = thue.matches(text);
         if (matches.length === 0) return 1.0; // will defintely halt.
@@ -15,9 +15,9 @@ function halts(thue, timeout = 5000) {
             if (mematch > 0)
                 chances.push(1.0 - (mematch / matches.length)); // may match itself this many times, so it might not halt.
             else
-                chances.push(chanceOfHalting(thue.apply(r, m, text, true)));
+                chances.push(hc(thue.apply(r, m, text, true)));
         }
         return chances.reduce((a, b) => a + b, 0.0) / chances.length;
     }
-    return chanceOfHalting(thue.text);
+    return hc(thue.text);
 }
