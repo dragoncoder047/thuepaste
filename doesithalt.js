39015b744c45e030f553cf801d2c08cbc6f8468d
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-11 11:28:17 -0500

    Update doesithalt.js

--- doesithalt.js
+++ doesithalt.js
@@ -1,24 +1,23 @@
 function chanceOfHalting(thue, timeout = 5000) {
     const start = +new Date();
-    function hc(text, seenRules) {
+    var seen = {}; ////////////////////////////////////
+    function hc(text) {
         if (+new Date() - start > timeout) throw 'timeout';
         var matches = thue.matches(text);
         if (matches.length === 0) return 1.0; // will defintely halt.
         var chances = [];
-        for (var [r, m] of matches) {
+        for (var i = 0; i < matches.length; i++) {
+            var [r, m] = matches[i];
+            // TODO - FINISH HERE
             var applied = thue.apply(r, m, text, true);
             // test to see if the rule will match itself or in a cycle
             var oldRules = thue.rules;
-            var moreSeen = seenRules.indexOf(r) === -1 ? [r].concat(seenRules) : seenRules;
+            var moreSeen = seenRules.indexOf(r) === -1 ? [r].concat(seenRules) : seenRules; // prevent duplicates
             thue.rules = moreSeen;
-            var cmatch = thue.matches(applied).length;
+            chances.push(hc(thue.apply(r, m, text, true)));
             thue.rules = oldRules;
-            if (cmatch > 0)
-                chances.push(1.0 - (cmatch / matches.length)); // may match itself this many times, so it might not halt.
-            else
-                chances.push(hc(thue.apply(r, m, text, true), moreSeen));
         }
         return chances.reduce((a, b) => a + b, 0.0) / chances.length;
     }
-    return hc(thue.text, []);
+    return hc(thue.text);
 }
