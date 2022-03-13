1ef7c297e37747d7b44bb84a598bc8498369f738
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-12 19:02:00 -0500

    todo: finish

--- doesithalt.js
+++ doesithalt.js
@@ -1,23 +1,22 @@
-function chanceOfHalting(thue, timeout = 5000) {
+function chanceOfHalting(thue, depthCallback, timeout = 5000) {
     const start = +new Date();
-    var seen = {}; ////////////////////////////////////
-    function hc(text) {
+    var seen = {};
+    function test(text, depth, path) {
         if (+new Date() - start > timeout) throw 'timeout';
         var matches = thue.matches(text);
         if (matches.length === 0) return 1.0; // will defintely halt.
         var chances = [];
-        for (var i = 0; i < matches.length; i++) {
-            var [r, m] = matches[i];
-            // TODO - FINISH HERE
+        for (var [r, m] of matches) {
+            if (depthCallback) depthCallback(depth);
             var applied = thue.apply(r, m, text, true);
-            // test to see if the rule will match itself or in a cycle
-            var oldRules = thue.rules;
-            var moreSeen = seenRules.indexOf(r) === -1 ? [r].concat(seenRules) : seenRules; // prevent duplicates
-            thue.rules = moreSeen;
-            chances.push(hc(thue.apply(r, m, text, true)));
-            thue.rules = oldRules;
+            for (var s in seen) {
+                if (s.indexOf(applied) > -1) {
+                    var c = seen[s];
+                    ////////////////////////////////////////
+                }
+            }
         }
         return chances.reduce((a, b) => a + b, 0.0) / chances.length;
     }
-    return hc(thue.text);
+    return test(thue.text, 0, [thue.text]);
 }
