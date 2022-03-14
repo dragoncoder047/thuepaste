b22937fbc940c117dc0f5443e8e47aeb25c4ffb5
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-14 07:45:43 -0400

    Update doesithalt.js

--- doesithalt.js
+++ doesithalt.js
@@ -4,18 +4,20 @@ function chanceOfHalting(thue, depthCallback, timeout = 5000) {
     function test(text, depth, path) {
         if (+new Date() - start > timeout) throw 'timeout';
         var matches = thue.matches(text);
+        seen[text] = matches.length;
         if (matches.length === 0) return 1.0; // will defintely halt.
         var chances = [];
         for (var [r, m] of matches) {
             if (depthCallback) depthCallback(depth);
             var applied = thue.apply(r, m, text, true);
             for (var s in seen) {
-                if (s.indexOf(applied) > -1) {
-                    var c = seen[s];
-                    throw 'todo'; ////////////////////////////////////////
+                var c = seen[s];
+                if (s === applied) {
+                    ////////////////////////////////////////////////////////////////////////////
                 }
             }
         }
+        throw 'todo'; ////////////////////////////////////////////////////
         return chances.reduce((a, b) => a + b, 0.0) / chances.length;
     }
     return test(thue.text, 0, [thue.text]);
