0c8fb9e8fb0867591a6cd86be2ab52829340b24c
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-14 14:31:33 -0400

    Update doesithalt.js

--- doesithalt.js
+++ doesithalt.js
@@ -1,23 +1,26 @@
 function chanceOfHalting(thue, depthCallback, abortSignal) {
     const start = +new Date();
-    var seen = {};
+    var seen = new Map();
     function test(text, depth, path) {
         if (abortSignal && abortSignal.aborted) throw abortSignal.reason || 'aborted';
+        
         var matches = thue.matches(text);
-        seen[text] = matches.length;
+        seen.set(text, matches.length);
         if (matches.length === 0) return 1.0; // will defintely halt.
+        
         var chances = [];
         for (var [r, m] of matches) {
             if (depthCallback) depthCallback(depth);
-            var applied = thue.apply(r, m, text, true);
-            for (var s in seen) {
-                var c = seen[s];
+            
+            var applied = thue.apply(r, m, text, true), found = false;
+            for (var [s, c] in seen) {
                 if (s === applied) {
-                    ////////////////////////////////////////////////////////////////////////////
+                    // is this really the best approximation?
+                    var ch = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa';
+                    throw 'todo';
                 }
             }
         }
-        throw 'todo'; ////////////////////////////////////////////////////
         return chances.reduce((a, b) => a + b, 0.0) / chances.length;
     }
     return test(thue.text, 0, [thue.text]);
