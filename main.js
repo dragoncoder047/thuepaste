dcb9ef1eefbcd9349668810141ab8754699214ae
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-16 08:20:20 -0400

    remove unnecessary `setTimeout`

--- main.js
+++ main.js
@@ -104,7 +104,7 @@ function determineHalts() {
             haltsButton.textContent = 'Abort';
             var chance;
             try {
-                chance = await chanceOfHalting(thue, depth => new Promise(r => {status(`Computing... depth ${depth}`, 'computing'); setTimeout(r, 10)}), haltsAborter.signal);
+                chance = await chanceOfHalting(thue, d => new Promise(r => {status(`Computing... depth ${d}`, 'computing'); r()}), haltsAborter.signal);
             } catch (e) {
                 status('Error: ' + e, 'error');
                 haltsAborter = null;
