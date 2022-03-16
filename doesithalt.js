995afde9283f204291c02ae803056375d4ec21fe
dragoncoder047 <101021094+dragoncoder047@users.noreply.github.com>
2022-03-16 11:16:43 -0400

    try new algorithm (part a)

--- doesithalt.js
+++ doesithalt.js
@@ -1,7 +1,52 @@
-async function chanceOfHalting(thue, depthCallback, abortSignal) {
-    var seen = new Map();
-    async function test(text, depth, path) {
+async function chanceOfHalting(thue, statusCallback, abortSignal) {
+    function interrupt() {
         if (abortSignal && abortSignal.aborted) throw abortSignal.reason || 'aborted';
+    }
+    
+    var tree = new Map();
+    tree.set(thue.text, thue.matches(thue.text));
+    var longLimit = 10000;
+    
+    async function findCycleFrom(text, path = []) {
+        // Brent's cycle finding algorithm
+        function foo(text, iii) {
+            var match = tree.get(text)[path[iii]];
+            if (match === undefined) return undefined;
+            return thue.apply(match[0], match[1], text, true);
+        }
+        var tortoise = text, hare = foo(text, path[0]);
+        var power = 1, lambda = 1;
+        while (tortoise !== hare) {
+            interrupt();
+            if (power === lambda) {
+                tortoise = hare;
+                power *= 2;
+                lambda = 0;
+            }
+            hare = foo(hare, lambda);
+            if (hare === undefined) return [undefined, undefined];
+            lambda++;
+            await statusCallback('cyclefind' + lambda);
+        }
+        tortoise = hare = text;
+        var i;
+        for (i = 0; i < lambda; i++)
+            hare = foo(hare, i);
+        var mu = 0;
+        while (tortoise !== hare) {
+            tortoise = foo(tortoise, mu);
+            hare = foo(hare, i++);
+            mu++;
+            await statusCallback('mufind' + mu);
+        }
+        return [lambda, mu];        
+    }
+    
+    return findCycleFrom(thue.text, [0, 0, 0, 0, 0])[0];
+    
+    /*var seen = new Map();
+    async function test(text, depth, path) {
+        interrupt();
         
         var matches = thue.matches(text);
         seen.set(text, matches.length);
@@ -9,7 +54,7 @@ async function chanceOfHalting(thue, depthCallback, abortSignal) {
         
         var chances = [];
         for (var [r, m] of matches) {
-            if (depthCallback) await depthCallback(depth);
+            if (statusCallback) await statusCallback(depth);
             
             var applied = thue.apply(r, m, text, true), found = false;
             for (var [s, c] in seen) {
@@ -23,12 +68,12 @@ async function chanceOfHalting(thue, depthCallback, abortSignal) {
                     chances.push(ch);
                     found = true;
                 }
-                //await depthCallback(depth);
+                //await statusCallback(depth);
             }
             // brute force recursive search
             if (!found) chances.push(await test(applied, depth + 1, path.concat([applied])));
         }
         return chances.reduce((a, b) => a + b, 0.0) / chances.length;
     }
-    return await test(thue.text, 0, [thue.text]);
+    return await test(thue.text, 0, [thue.text]);*/
 }
